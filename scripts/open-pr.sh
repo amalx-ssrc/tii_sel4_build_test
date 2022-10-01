91a5d69afacb3f8e9f384f50f18048885dd33ced
amalx-ssrc <amalx@ssrc.tii.ae>
2022-10-01 16:56:25 +0530

    pushes

--- scripts/open-pr.sh
+++ scripts/open-pr.sh
@@ -5,9 +5,9 @@ sel4_repos=(seL4_tools camkes-tool gitactions-pr)
 PR_BR_NAME=$1
 same_repocheck () {
       repo=$1  
-      pr_repo_owner=$(curl  https://api.github.com/repos/amalx-ssrc/${repo}/pulls?state=open -H "Authorization: Bearer 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" -H "Accept: application/json" | jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .user.login')
-      repo_owner=$(curl  https://api.github.com/repos/amalx-ssrc/${repo}/pulls?state=open -H "Authorization: Bearer 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" -H "Accept: application/json" | jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .base.user.login')
-      repo_name=$(curl  https://api.github.com/repos/amalx-ssrc/${repo}/pulls?state=open -H "Authorization: Bearer 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" -H "Accept: application/json" | jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .head.repo.name')
+      pr_repo_owner=$(curl  https://api.github.com/repos/amalx-ssrc/${repo}/pulls?state=open -H "Accept: application/json" | jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .user.login')
+      repo_owner=$(curl  https://api.github.com/repos/amalx-ssrc/${repo}/pulls?state=open -H "Accept: application/json" | jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .base.user.login')
+      repo_name=$(curl  https://api.github.com/repos/amalx-ssrc/${repo}/pulls?state=open -H "Accept: application/json" | jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .head.repo.name')
       if [ $pr_repo_owner != $repo_owner ]; then 
       echo "pr from same repo"
       echo $repo
@@ -22,7 +22,7 @@ same_repocheck () {
 same_branch=()
 for repo in ${sel4_repos[@]}; do 
 #if [ $PR_REPO_NAME = $OR_REPO_NAME ]
-branch_name=$(curl https://api.github.com/repos/amalx-ssrc/${repo}/pulls?state=open -H "Authorization: Bearer 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" -H "Accept: application/json" | jq -r '.[] | .head.ref')
+branch_name=$(curl https://api.github.com/repos/amalx-ssrc/${repo}/pulls?state=open -H "Accept: application/json" | jq -r '.[] | .head.ref')
 
 #echo $branch_name"amal"
         for name in ${branch_name[@]}; do 
