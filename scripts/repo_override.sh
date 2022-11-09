de510e6cff4b00d649e8d2fe2cc3e8a0136b5e03
amalx-ssrc <amalx@ssrc.tii.ae>
2022-11-09 15:42:05 +0530

    curl and jq added to dockefile

--- scripts/repo_override.sh
+++ scripts/repo_override.sh
@@ -21,10 +21,10 @@ same_repocheck () {
       repo_owner=$(jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .base.user.login' api_file)
       repo_name=$(jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .head.repo.name' api_file)
       if [ $pr_repo_owner != $repo_owner ]; then 
-      sed -i "/<manifest>/a <remote name=\"$pr_repo_owner\" fetch=\"https://github.com/$pr_repo_owner\"/>" tii_sel4_manifest-$PR_BR_NAME/external.xml
+      sed -i "/<manifest>/a <remote name=\"$pr_repo_owner\" fetch=\"https://github.com/$pr_repo_owner\"/>" $MANIFEST_PATH
       #sed -i "/<manifest>/a <remote name=\"$pr_repo_owner\" fetch=\"https://github.com/$pr_repo_owner\"/>" ./external.xml
       fi
-      sed -i "/$repo.git/c\<extend-project name=\"$repo_name.git\"                remote=\"$pr_repo_owner\" revision=\"$PR_BR_NAME\"/>" tii_sel4_manifest-$PR_BR_NAME/external.xml
+      sed -i "/$repo.git/c\<extend-project name=\"$repo_name.git\"                remote=\"$pr_repo_owner\" revision=\"$PR_BR_NAME\"/>" $MANIFEST_PATH
       #sed -i "/$repo.git/c\<extend-project name=\"$repo_name.git\"                remote=\"$pr_repo_owner\" revision=\"$PR_BR_NAME\"/>" ./external.xml
     }
 
