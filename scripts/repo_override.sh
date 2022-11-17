d45559fd576734328df407bbce4b038562e2b3cb
amalx-ssrc <amalx@ssrc.tii.ae>
2022-11-17 14:26:38 +0530

    created repo and branch override

--- scripts/repo_override.sh
+++ scripts/repo_override.sh
@@ -1,49 +1,70 @@
 #! /bin/bash
 
-REPO_OVERRIDE=$1
-MANIFEST_PATH=$2
+echo "repo_override      amalbabus/camkes-tool:main  " 
 
-# echo $REPO_OVERRIDE
-# echo $MANIFEST_PATH
+REPO_OVERRIDE= $1
 
-cat $MANIFEST_PATH
-
+PR_BR_NAME=$(echo $REPO_OVERRIDE | cut -d: -f2)
+repo_owner=$(echo $REPO_OVERRIDE | cut -d: -f1)
 
 
 
-owner=amalx-ssrc
-sel4_repos=(seL4_tools camkes-tool gitactions-pr)
-PR_BR_NAME=$1
-
-same_repocheck () {
-      repo=$1  
-      pr_repo_owner=$( jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .user.login' api_file )
-      repo_owner=$(jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .base.user.login' api_file)
-      repo_name=$(jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .head.repo.name' api_file)
-      if [ $pr_repo_owner != $repo_owner ]; then 
-      sed -i "/<manifest>/a <remote name=\"$pr_repo_owner\" fetch=\"https://github.com/$pr_repo_owner\"/>" $MANIFEST_PATH
-      #sed -i "/<manifest>/a <remote name=\"$pr_repo_owner\" fetch=\"https://github.com/$pr_repo_owner\"/>" ./external.xml
-      fi
-      sed -i "/$repo.git/c\<extend-project name=\"$repo_name.git\"                remote=\"$pr_repo_owner\" revision=\"$PR_BR_NAME\"/>" $MANIFEST_PATH
-      #sed -i "/$repo.git/c\<extend-project name=\"$repo_name.git\"                remote=\"$pr_repo_owner\" revision=\"$PR_BR_NAME\"/>" ./external.xml
-    }
-
-
-for repo in ${sel4_repos[@]}; do 
 curl  https://api.github.com/repos/${owner}/${repo}/pulls?state=open -H "Accept: application/json" > api_file
 
  branch_name=$(jq -r '.[] | .head.ref' api_file)
         for name in ${branch_name[@]}; do 
             if [ "$PR_BR_NAME" = "$name" ]; then
-            #same_branch+=($repo)
-            same_repocheck $repo
+                  pr_repo_owner=$( jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .user.login' api_file )
+                               
+                  if [ $pr_repo_owner != $repo_owner ]; then 
+                  sed -i "/<manifest>/a <remote name=\"$pr_repo_owner\" fetch=\"https://github.com/$pr_repo_owner\"/>" $MANIFEST_PATH
+                  #sed -i "/<manifest>/a <remote name=\"$pr_repo_owner\" fetch=\"https://github.com/$pr_repo_owner\"/>" ./external.xml
+                  fi
+                  sed -i "/$repo.git/c\<extend-project name=\"$repo_name.git\"                remote=\"$pr_repo_owner\" revision=\"$PR_BR_NAME\"/>" $MANIFEST_PATH
+                  #sed -i "/$repo.git/c\<extend-project name=\"$repo_name.git\"                remote=\"$pr_repo_owner\" revision=\"$PR_BR_NAME\"/>" ./external.xml
+
             fi
         done
-done
-
 
 echo "after"
 
 
 
 cat $MANIFEST_PATH
+
+
+
+
+
+
+
+
+# pr_repo_owner=$( jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .user.login' api_file )
+
+# repo_name=$(jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .head.repo.name' api_file)
+
+
+
+
+
+#       if [ $pr_repo_owner != $repo_owner ]; then 
+#       sed -i "/<manifest>/a <remote name=\"$pr_repo_owner\" fetch=\"https://github.com/$pr_repo_owner\"/>" $MANIFEST_PATH
+#       #sed -i "/<manifest>/a <remote name=\"$pr_repo_owner\" fetch=\"https://github.com/$pr_repo_owner\"/>" ./external.xml
+#       fi
+#       sed -i "/$repo.git/c\<extend-project name=\"$repo_name.git\"                remote=\"$pr_repo_owner\" revision=\"$PR_BR_NAME\"/>" $MANIFEST_PATH
+#       #sed -i "/$repo.git/c\<extend-project name=\"$repo_name.git\"                remote=\"$pr_repo_owner\" revision=\"$PR_BR_NAME\"/>" ./external.xml
+
+
+
+
+
+# if [repo_owner=pr_repo_owner]; then
+
+# # repo_owner=$(jq -r --arg PR_BR_NAME "$PR_BR_NAME" '.[] | select(.head.ref == $PR_BR_NAME) | .base.user.login' api_file)
+#  branch_name=$(jq -r '.[] | .head.ref' api_file)
+
+#  if [ "$PR_BR_NAME" = "$name" ]; then
+      
+     
+
+#    fi
